ea47335e1544a07a70edfe65c98f628820f859b7
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-09-18 21:54:29 +0700

    praktikum 2

---
+++ JS4/Praktikum2.dart
@@ -0,0 +1,14 @@
+void main() {
+var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+
+var names1 = <String>{};
+Set<String> names2 = {};
+var names3 = {}; // tetap ada, tapi tidak dipakai dulu
+
+names1.add("Ananda Rahma");
+names2.addAll(["2341720048", "Ananda Rahma"]); 
+
+print(names1); 
+print(names2); 
+}
