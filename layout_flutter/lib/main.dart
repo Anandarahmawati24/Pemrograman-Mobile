52864fd8d379562c9b5719c12110b0b58c20c73f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-10-04 23:42:38 +0700

    praktikum 2 Jobsheet 6

--- layout_flutter/lib/main.dart
+++ layout_flutter/lib/main.dart
@@ -3,55 +3,80 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
   Widget get titleSection => Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /* soal 1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /* soal 2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Wisata Gunung di Batu',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-            ),
-            const Text(
-              'Batu, Malang, Indonesia',
-              style: TextStyle(
-                color: Colors.grey,
+              const Text(
+                'Batu, Malang, Indonesia',
+                style: TextStyle(color: Colors.grey),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      /* soal 3*/
-      const Icon(
-       Icons.star,
-        color: Colors.red,
-      ),
-      const Text('41'),
-    ],
-  ),
-);
-  @override
+        /* soal 3*/
+        const Icon(Icons.star, color: Colors.red),
+        const Text('41'),
+      ],
+    ),
+  );
+
+ @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Ananda Rahmawati (2341720048)',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: Column(children: [titleSection, buttonSection]),
+      ),
+    );
+  }
+
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
         ),
-        body: titleSection,
-        ),
-      );
+      ],
+    );
   }
 }
