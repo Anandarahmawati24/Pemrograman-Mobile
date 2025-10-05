efb019b3a9efea1e50509fc7d5810ef44c5d28ba
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-10-05 14:51:42 +0700

    Praktikum 5: Membangun Navigasi di Flutter Langkah 7

--- belanja/lib/pages/item_page.dart
+++ belanja/lib/pages/item_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final item = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(item.name)),
+      body: Center(
+        child: Text(
+          'Harga: Rp ${item.price}',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
