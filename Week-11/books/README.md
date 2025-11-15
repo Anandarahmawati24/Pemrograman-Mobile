d3fe251fa1304b78aa0cd502bb5aaff2f26b71c6
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-11-15 22:28:28 +0700

    W11: Soal 3

--- Week-11/books/README.md
+++ Week-11/books/README.md
@@ -8,3 +8,8 @@
 ### Soal 2
 ![Layout](images/prak1_1.png)
 ![Layout](images/prak1_2.png)
+
+### Soal 3 Jelaskan maksud kode langkah 5 tersebut terkait substring dan catchError!
+**Jawab:** Kode pada langkah 5 menggunakan substring(0, 450) untuk mengambil 450 karakter pertama dari data respons API. Tujuannya agar teks yang ditampilkan tidak terlalu panjang dan tetap rapi di layar. Sementara itu, catchError digunakan untuk menangani kesalahan yang terjadi ketika proses pengambilan data gagal, seperti masalah jaringan atau server. Jika error muncul, nilai result akan diganti dengan pesan "An error occurred" dan UI diperbarui agar pengguna mengetahui bahwa terjadi kesalahan.
+
+![Layout](images/prak1_3.gif)
--- Week-11/books/lib/main.dart
+++ Week-11/books/lib/main.dart
@@ -42,8 +42,19 @@ class _FuturePageState extends State<FuturePage> {
           children: <Widget>[
             const Spacer(),
             ElevatedButton(
-              onPressed: () {}, // Fungsi onPressed masih kosong
-              child: const Text('GO!'),
+              child: Text('GO!'),
+              onPressed: () {
+                setState(() {});
+                getData()
+                    .then((value) {
+                      result = value.body.toString().substring(0, 450);
+                      setState(() {});
+                    })
+                    .catchError((_) {
+                      result = 'An error occurred';
+                      setState(() {});
+                    });
+              },
             ),
             const Spacer(),
             Text(result),
